e2e8a58609ae0cdd57078f843005f9f0f0d0154b
Azadan <azadan@live.com>
2025-09-05 00:09:20 +0200

    Player controller

--- multiplayer-grupp1/src/main/java/com/multiplayer_grupp1/multiplayer_grupp1/controller/PlayerController.java
+++ multiplayer-grupp1/src/main/java/com/multiplayer_grupp1/multiplayer_grupp1/controller/PlayerController.java
@@ -1,4 +1,12 @@
 package com.multiplayer_grupp1.multiplayer_grupp1.controller;
 
+import com.multiplayer_grupp1.multiplayer_grupp1.service.PlayerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
 public class PlayerController {
+
+    private final PlayerService playerService;
 }
