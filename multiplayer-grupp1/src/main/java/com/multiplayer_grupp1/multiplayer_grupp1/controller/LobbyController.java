95c4f77a87d564734cd0cdaae1cd2eb7f48afc73
Azadan <azadan@live.com>
2025-09-05 00:08:32 +0200

    Lobby controller

--- multiplayer-grupp1/src/main/java/com/multiplayer_grupp1/multiplayer_grupp1/controller/LobbyController.java
+++ multiplayer-grupp1/src/main/java/com/multiplayer_grupp1/multiplayer_grupp1/controller/LobbyController.java
@@ -1,4 +1,12 @@
 package com.multiplayer_grupp1.multiplayer_grupp1.controller;
 
-public class LobbyService {
+import com.multiplayer_grupp1.multiplayer_grupp1.service.LobbyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LobbyController {
+
+    private final LobbyService lobbyService;
 }
