419db216a231b972e7bf2be507fbefa496483480
Azadan <azadan@live.com>
2025-09-09 00:42:10 +0200

    Add ConcurrentHashMap

--- multiplayer-grupp1/src/main/java/com/multiplayer_grupp1/multiplayer_grupp1/service/PlayerService.java
+++ multiplayer-grupp1/src/main/java/com/multiplayer_grupp1/multiplayer_grupp1/service/PlayerService.java
@@ -1,7 +1,16 @@
 package com.multiplayer_grupp1.multiplayer_grupp1.service;
 
+import com.multiplayer_grupp1.multiplayer_grupp1.model.Player;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 @Service
+@RequiredArgsConstructor
 public class PlayerService {
+
+    private final Map<String, Player> players = new ConcurrentHashMap<>();
+
 }
