7c9094119975ed16c7816b7672fcc04bf841ed16
Azadan <azadan@live.com>
2025-09-09 00:43:09 +0200

    add playeNotFoundException

--- multiplayer-grupp1/src/main/java/com/multiplayer_grupp1/multiplayer_grupp1/Exceptions/PlayerNotFoundException.java
+++ multiplayer-grupp1/src/main/java/com/multiplayer_grupp1/multiplayer_grupp1/Exceptions/PlayerNotFoundException.java
@@ -1,4 +1,7 @@
 package com.multiplayer_grupp1.multiplayer_grupp1.Exceptions;
 
-public class PlayerNotFoundException {
+public class PlayerNotFoundException extends RuntimeException {
+    public PlayerNotFoundException(String message) {
+        super(message);
+    }
 }
