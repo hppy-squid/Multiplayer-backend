ebc6c4900d7ddd74ad5bbff2c90766619b212c75
Azadan <azadan@live.com>
2025-09-08 23:38:23 +0200

    Throw custom exception

--- multiplayer-grupp1/src/main/java/com/multiplayer_grupp1/multiplayer_grupp1/service/QuestionService.java
+++ multiplayer-grupp1/src/main/java/com/multiplayer_grupp1/multiplayer_grupp1/service/QuestionService.java
@@ -1,5 +1,6 @@
 package com.multiplayer_grupp1.multiplayer_grupp1.service;
 
+import com.multiplayer_grupp1.multiplayer_grupp1.Exceptions.QuestionNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,7 +16,7 @@ public class QuestionService {
 
     //Denna bör hämta frågan och svarsalternativen
     public Question getQuestionById(long id) {
-        return questionRepository.findById(id).orElse(null);
+        return questionRepository.findById(id).orElseThrow(() -> new QuestionNotFoundException("Question with this id not found"));
     }
 
     /*
