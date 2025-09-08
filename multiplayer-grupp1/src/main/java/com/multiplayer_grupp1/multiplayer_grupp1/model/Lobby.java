02fc0a82f367ed70ddd65ed52e36c4ae4ff60121
Azadan <azadan@live.com>
2025-09-08 23:30:24 +0200

    LobbyCode as random UUID and list<player> max 4

--- multiplayer-grupp1/src/main/java/com/multiplayer_grupp1/multiplayer_grupp1/model/Lobby.java
+++ multiplayer-grupp1/src/main/java/com/multiplayer_grupp1/multiplayer_grupp1/model/Lobby.java
@@ -4,21 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
-import jakarta.persistence.Entity;
-
-@Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class Lobby {
 
-    private String lobbyCode;
+    private String lobbyCode = UUID.randomUUID().toString();
 
     private int maxPlayers;
 
-    private List<Player> players;
+    private List<Player> players = new ArrayList<>(4);
 
     private GameState gameState;
 
