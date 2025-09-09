34a5db45f86135d0121fa011e8c97ced1ff42933
Azadan <azadan@live.com>
2025-09-09 21:53:54 +0200

    playerrepository

--- multiplayer-grupp1/src/main/java/com/multiplayer_grupp1/multiplayer_grupp1/repository/PlayerRepository.java
+++ multiplayer-grupp1/src/main/java/com/multiplayer_grupp1/multiplayer_grupp1/repository/PlayerRepository.java
@@ -1,4 +1,13 @@
 package com.multiplayer_grupp1.multiplayer_grupp1.repository;
 
-public interface PlayerRepository {
+import com.multiplayer_grupp1.multiplayer_grupp1.model.Player;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface PlayerRepository extends JpaRepository<Player, Long> {
+
+    Player findPlayerById(Long id);
+
+    boolean existsByPlayerName(String playerName);
 }
