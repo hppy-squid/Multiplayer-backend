17b4e896c83a7c960378ec4d99c108144c69d470
Azadan <azadan@live.com>
2025-09-09 21:54:10 +0200

    create player method

--- multiplayer-grupp1/src/main/java/com/multiplayer_grupp1/multiplayer_grupp1/service/PlayerService.java
+++ multiplayer-grupp1/src/main/java/com/multiplayer_grupp1/multiplayer_grupp1/service/PlayerService.java
@@ -1,16 +1,27 @@
 package com.multiplayer_grupp1.multiplayer_grupp1.service;
 
+import com.multiplayer_grupp1.multiplayer_grupp1.Exceptions.PlayerAlreadyExists;
 import com.multiplayer_grupp1.multiplayer_grupp1.model.Player;
+import com.multiplayer_grupp1.multiplayer_grupp1.repository.PlayerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+
 
 @Service
 @RequiredArgsConstructor
 public class PlayerService {
 
-    private final Map<String, Player> players = new ConcurrentHashMap<>();
+    private final PlayerRepository playerRepository;
+
+
+    public Player createPlayer(Player player){
+
+            if (playerRepository.existsByPlayerName(player.getPlayerName())) {
+                throw new PlayerAlreadyExists("Player with name " + player.getPlayerName() + " already exists.");
+            }
+            return playerRepository.save(player);
+
+    }
 
 }
