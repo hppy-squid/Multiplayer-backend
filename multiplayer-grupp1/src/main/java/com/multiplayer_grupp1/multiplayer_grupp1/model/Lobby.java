d19b68109488f003176c30deb991f01f349b44bb
Azadan <azadan@live.com>
2025-09-10 21:06:01 +0200

    refactor to entity and randomAlphanumeric

--- multiplayer-grupp1/src/main/java/com/multiplayer_grupp1/multiplayer_grupp1/model/Lobby.java
+++ multiplayer-grupp1/src/main/java/com/multiplayer_grupp1/multiplayer_grupp1/model/Lobby.java
@@ -1,21 +1,28 @@
 package com.multiplayer_grupp1.multiplayer_grupp1.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+
 import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.RandomStringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
+
+
+@Entity
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
 public class Lobby {
 
-    // Blir alltför långt om meningen är att detta är koden spelarna joinar med, bör nog ha 6-teckn lobbykod
-    private String lobbyCode = UUID.randomUUID().toString();
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    private String lobbyCode = RandomStringUtils.randomAlphanumeric(6).toUpperCase();
+
+    @OneToMany
     private List<Player> players = new ArrayList<>(4);
 
     private GameState gameState;
