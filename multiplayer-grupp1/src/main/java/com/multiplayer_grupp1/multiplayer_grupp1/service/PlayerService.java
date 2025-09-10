ca2afa6aa1a521d97fdf706bed967eaf8752d86d
Azadan <azadan@live.com>
2025-09-10 21:44:46 +0200

    DTO methods and nrefactored createplayer

--- multiplayer-grupp1/src/main/java/com/multiplayer_grupp1/multiplayer_grupp1/service/PlayerService.java
+++ multiplayer-grupp1/src/main/java/com/multiplayer_grupp1/multiplayer_grupp1/service/PlayerService.java
@@ -1,10 +1,13 @@
 package com.multiplayer_grupp1.multiplayer_grupp1.service;
 
+import org.springframework.stereotype.Service;
+
+import com.multiplayer_grupp1.multiplayer_grupp1.Dto.PlayerDTO;
 import com.multiplayer_grupp1.multiplayer_grupp1.Exceptions.PlayerAlreadyExists;
 import com.multiplayer_grupp1.multiplayer_grupp1.model.Player;
 import com.multiplayer_grupp1.multiplayer_grupp1.repository.PlayerRepository;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
@@ -13,13 +16,28 @@ public class PlayerService {
     private final PlayerRepository playerRepository;
 
 
-    public Player createPlayer(Player player){
-
-            if (playerRepository.existsByPlayerName(player.getPlayerName())) {
-                throw new PlayerAlreadyExists("Player with name " + player.getPlayerName() + " already exists.");
-            }
-            return playerRepository.save(player);
-
+    private Player ConvertToEntity(PlayerDTO playerDTO) {
+        Player player = new Player();
+        player.setId(playerDTO.id());
+        player.setPlayerName(playerDTO.playerName());
+        player.setScore(playerDTO.score());
+        player.setHost(playerDTO.isHost());
+        return player;
     }
 
+    private PlayerDTO ConvertToDTO(Player player) {
+        return new PlayerDTO(
+                player.getId(),
+                player.getPlayerName(),
+                player.getScore(),
+                player.isHost());
+    }
+
+    public PlayerDTO createPlayer(Player player) {
+        if (playerRepository.existsByPlayerName(player.getPlayerName())) {
+            throw new PlayerAlreadyExists("Player with name " + player.getPlayerName() + " already exists.");
+        }
+        Player newPlayer = playerRepository.save(player);
+        return ConvertToDTO(newPlayer);
+    }
 }
