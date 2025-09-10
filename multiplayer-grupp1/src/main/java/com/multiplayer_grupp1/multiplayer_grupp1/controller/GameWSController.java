8fb2632a2a9f86f746adbabebd31dac7f1899333
YLudvig <yslu24ne@student.ju.se>
2025-09-10 18:15:17 +0200

    websocketstart

---
+++ multiplayer-grupp1/src/main/java/com/multiplayer_grupp1/multiplayer_grupp1/controller/GameWSController.java
@@ -0,0 +1,28 @@
+package com.multiplayer_grupp1.multiplayer_grupp1.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+import com.multiplayer_grupp1.multiplayer_grupp1.service.GameService;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class GameWSController {
+
+    private final GameService gameService;
+
+    /* // Mapping för att skicka att användare är redo i lobbyn
+    @MessageMapping("/game/{lobbyCode}")
+    @SendTo("/ready")
+
+    // Mapping för att skicka att användare har svarat på frågan 
+    @MessageMapping("/game/{lobbyCode}")
+    @SendTo("/response")
+
+    // Mapping för att uppdatera tiden 
+    @MessageMapping("/game/{lobbyCode}")
+    @SendTo("/timer") */
+}
