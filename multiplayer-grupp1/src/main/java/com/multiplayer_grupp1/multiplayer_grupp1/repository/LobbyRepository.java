fe07e5d3cece5860f259b516045df96f1573a5ab
Azadan <azadan@live.com>
2025-09-10 21:07:01 +0200

    lobby service and repository

--- multiplayer-grupp1/src/main/java/com/multiplayer_grupp1/multiplayer_grupp1/repository/LobbyRepository.java
+++ multiplayer-grupp1/src/main/java/com/multiplayer_grupp1/multiplayer_grupp1/repository/LobbyRepository.java
@@ -1,4 +1,11 @@
 package com.multiplayer_grupp1.multiplayer_grupp1.repository;
 
-public class LobbyRepository {
+import com.multiplayer_grupp1.multiplayer_grupp1.model.Lobby;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LobbyRepository extends JpaRepository<Lobby, Long> {
+
+    boolean existsByLobbyName(String lobbyName);
+
+    Lobby findByLobbyCode(String lobbyCode);
 }
--- multiplayer-grupp1/src/main/java/com/multiplayer_grupp1/multiplayer_grupp1/service/LobbyService.java
+++ multiplayer-grupp1/src/main/java/com/multiplayer_grupp1/multiplayer_grupp1/service/LobbyService.java
@@ -1,9 +1,19 @@
 package com.multiplayer_grupp1.multiplayer_grupp1.service;
 
+import com.multiplayer_grupp1.multiplayer_grupp1.repository.LobbyRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Random;
+
 @Service
+@RequiredArgsConstructor
 public class LobbyService {
+
+    private final PlayerService playerService;
+    private final LobbyRepository lobbyRepository;
+
+
 
 
 }
