3c6d1bb79d7c806c91046af861845e07b3ed5e45
YLudvig <yslu24ne@student.ju.se>
2025-09-11 18:20:49 +0200

    readycheck ws nära klar

--- multiplayer-grupp1/src/main/java/com/multiplayer_grupp1/multiplayer_grupp1/model/Game.java
+++ multiplayer-grupp1/src/main/java/com/multiplayer_grupp1/multiplayer_grupp1/model/Game.java
@@ -1,11 +1,15 @@
 package com.multiplayer_grupp1.multiplayer_grupp1.model;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
-@Data
+@Getter
+@Setter
 @RequiredArgsConstructor
 public class Game {
 
--- multiplayer-grupp1/src/main/java/com/multiplayer_grupp1/multiplayer_grupp1/service/GameService.java
+++ multiplayer-grupp1/src/main/java/com/multiplayer_grupp1/multiplayer_grupp1/service/GameService.java
@@ -2,7 +2,16 @@ package com.multiplayer_grupp1.multiplayer_grupp1.service;
 
 import org.springframework.stereotype.Service;
 
+import com.multiplayer_grupp1.multiplayer_grupp1.model.Ready;
+
 @Service
 public class GameService {
+
+    public Ready toggleReady(Ready readyMsg) {
+        // Bör toggla till det den inte är (detta gör den i teori återanvändbar om vi vill möjliggöra att toggla ready och inte ready)
+        readyMsg.setReady(!readyMsg.isReady());
+
+        return readyMsg;
+    }
     
 }
