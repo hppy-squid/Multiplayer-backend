4cf0ed2c563b017123a47fc100448f986e75282b
Azadan <azadan@live.com>
2025-09-12 20:36:41 +0200

    answer repository

--- multiplayer-grupp1/src/main/java/com/multiplayer_grupp1/multiplayer_grupp1/repository/PlayerAnswerRepository.java
+++ multiplayer-grupp1/src/main/java/com/multiplayer_grupp1/multiplayer_grupp1/repository/PlayerAnswerRepository.java
@@ -1,4 +1,16 @@
 package com.multiplayer_grupp1.multiplayer_grupp1.repository;
 
-public interface PlayerAnswerRepository {
+import com.multiplayer_grupp1.multiplayer_grupp1.model.PlayerAnswer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PlayerAnswerRepository extends JpaRepository<PlayerAnswer, Long> {
+    List<PlayerAnswer> findByGameIdAndQuestionQuestionIdOrderByAnsweredAtAsc(Long gameId, Long questionId);
+
+    boolean existsByGameIdAndPlayerIdAndQuestionQuestionId(Long gameId, Long playerId, Long questionId);
+
+    int countByGameIdAndQuestionQuestionId(Long gameId, Long questionId);
+
+    List<PlayerAnswer> findByGameIdAndQuestionQuestionId(Long gameId, Long questionId);
 }
